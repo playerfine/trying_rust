5bc3eb01b317314e7a2a30cc3a3a7a09877e86d4
Lon SQlapius <lonsqlapis@Lons-iMac.local>
2021-11-09 09:46:27 -0400

    finished rust guessing game

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,32 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number);
+//    println!("The secret number is: {}", secret_number);
 
-    println!("PLease input your guess");
+    loop {
+        println!("PLease input your guess");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Faled to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Faled to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 =  match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {} ", guess);
+        println!("You guessed: {} ", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            } 
+        }
+
     }
 }
